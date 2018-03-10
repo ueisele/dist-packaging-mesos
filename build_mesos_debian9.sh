6a6374243b1e33943ee7770bd4b1f1a1df8b878b
Uwe Eisele <uwe.eisele@online.de>
2018-03-10 16:47:59 +0100

    checkout mesos repo in docker container

--- build_mesos_debian9.sh
+++ build_mesos_debian9.sh
@@ -6,12 +6,12 @@ CWD_ROOT=`pwd`
 popd > /dev/null
 
 function prepare() {
-    docker build -t mesos-packaging:debian9 ${CWD_ROOT}/docker/debian9
+    docker build -t dist-packaging-mesos:debian9 ${CWD_ROOT}/docker/debian9
 }
 
 function build_mesos() {
     local version=${1:-1.5.0}
-    docker run --rm -v ${CWD_ROOT}:/mesos-deb-packaging -e VERSION=${version} mesos-packaging:debian9
+    docker run --rm -v ${CWD_ROOT}:/work/dist-packaging-mesos -e VERSION=${version} dist-packaging-mesos:debian9
 }
 
 if [ "${BASH_SOURCE[0]}" == "$0" ]; then
